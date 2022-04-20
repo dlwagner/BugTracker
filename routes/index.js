b38d318472686619bec934bc2da4e24941c93822
dlwagner <wagner.don@gmail.com>
2022-04-20 12:58:14 -0500

    add dataTables, new bug form, get data going to db and back to main bug table

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,37 @@ const express = require('express')
 const router = express.Router()
 const Bug = require('../models/bug')
 
-// Index Route: This is the root of the application
-router.get('/', (req, res) => {
-    res.render('index')
+//all bugs route
+router.get('/', async (req, res) => {
+    try {
+        const bugs = await Bug.find({})
+        res.render('index/bugTable', { bugs: bugs })
+    } catch {
+        res.redirect('/')
+    }
+    //res.render('index/bugTable')
 })
 
-// New Bug Route
+//new bug route
 router.get('/new', (req, res) => {
+    //res.send('new bug page')
     res.render('index/new', { bug: new Bug() })
 })
 
-// Create Bug Route
-router.post('/', (req, res) => {
-    //res.send('Create New Bug Report')
-    //res.render('index')
-    res.send(req.body.name)
+//create new bug route
+router.post('/', async (req, res) => {
+    const bug = new Bug({
+        name: req.body.name
+    })
+    try {
+        const newBug = await bug.save()
+        res.redirect('/')
+    } catch {
+        res.render('index/new', {
+            bug: bug,
+            errorMessage: 'Error creating bug report'
+        })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,24 +8,13 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 
-// Import routers into our server.js file
 const indexRouter = require('./routes/index')
-//const bugRouter = require('./routes/bugReport')
 
-// Set view engine
 app.set('view engine', 'ejs')
-
-// Set where our views are coming from
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-
-// Tell express to use express layouts
 app.use(expressLayouts)
-
-// Tell express where our public files will be
 app.use(express.static('public'))
-
-// Tell express to use body-parser
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // Setup Mongo database connection and check to see if we're connected or not.
@@ -35,8 +24,6 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-//Tell our app to use our index router
 app.use('/', indexRouter)
-//app.use('/bugs', bugRouter)
 
 app.listen(process.env.PORT || 3000)
